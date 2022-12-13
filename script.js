7519051114fb31067016141102cd84cca658d56d
alpayacr0 <alp330acr@gmail.com>
2022-12-13 18:04:14 +0300

    Zamanlayıcıda düzenleme

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let named = prompt("İsminizi giriniz.")
-named = named.charAt(0).toUpperCase()+named.slice(1);
+named = named.charAt(0).toUpperCase() + named.slice(1);
 let seçim = document.getElementById("myName").innerHTML = named
 
 function showTime() {
@@ -15,7 +15,7 @@ function showTime() {
         case 1:
             gun = "Pazartesi"
             break;
-        case 2: 
+        case 2:
             gun = "Sali"
             break;
         case 3:
@@ -31,10 +31,10 @@ function showTime() {
             gun = "Cumartesi"
             break;
     }
-    document.querySelectorAll(".clock")[0].innerHTML = kontrol(saat) + ":" + kontrol(dakika)+":"+kontrol(saniye)+" "+gun;
-    function kontrol(doğruluk){
-        if(doğruluk<10){doğruluk = "0"+doğruluk}
+    document.querySelectorAll(".clock")[0].innerHTML = kontrol(saat) + ":" + kontrol(dakika) + ":" + kontrol(saniye) + " " + gun;
+    function kontrol(doğruluk) {
+        if (doğruluk < 10) { doğruluk = "0" + doğruluk }
         return doğruluk;
     }
 }
-setInterval(showTime,100);
+setInterval(showTime, 1000);
